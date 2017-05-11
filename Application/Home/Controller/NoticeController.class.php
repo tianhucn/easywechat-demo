f051e9b0987e3f3634db8bab0bab9e1c6e0ff0ef
insua <insuaxe@gmail.com>
2017-05-11 09:59:59 +0800

    that coulda been bad

--- Application/Home/Controller/NoticeController.class.php
+++ Application/Home/Controller/NoticeController.class.php
@@ -26,4 +26,25 @@ class NoticeController extends WechatBaseController
 
         var_dump($result);
     }
+
+    public function color()
+    {
+        $app = new Application($this->options);
+
+        $notice = $app->notice;
+
+        $userId = 'oFBEXwAkPD7EYhPyzOIVKHPB2USY';
+        $templateId = '8G-OZkZADR2mwGhlNa50TqFxMq1H-VaN5M0wFn55Ac8';
+
+        $url = 'https://github.com';
+
+        $data = [
+            'poem' => ['苟利国家生死以', '#33FF99'],
+            'journalist' => ['华莱士', '#AA0011'],
+        ];
+
+        $result = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($userId)->send();
+
+        var_dump($result);
+    }
 }
